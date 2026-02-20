58302740e4ca9181585ed8d079eac5b3f5ca933b
otovson <chovanmat@gmail.com>
2026-02-20 11:03:15 +0100

    translate projects to English

--- practice4.py
+++ practice4.py
@@ -1,4 +1,4 @@
-meno = input("Set your name: ")
-vek = int(input("Set your age: "))
+name = input("Set your name: ")
+age = int(input("Set your age: "))
 
-print (f"Hi {meno}, 10 years ago you were {vek - 10} years old and 20 years from now your age will be {vek + 20} and your name has {len(meno)} characters")
+print (f"Hi {name}, 10 years ago you were {age - 10} years old and 20 years from now your age will be {age + 20} and your name has {len(name)} characters")
